80005a20bdeed839065d985530b8c6801eda903b
Mohamed Khaled <themidozedan@gmail.com>
2019-08-04 12:44:15 +0200

    Update dbConnection.js

--- routes/dbConnection.js
+++ routes/dbConnection.js
@@ -3,10 +3,10 @@ const mysql = require('mysql');
 
 
 const con = mysql.createConnection({
-    host: "sql7.freemysqlhosting.net",
+    host: "remotemysql.com",
     user: "jMN8bA3G1E",
     password: "XpeT2amIuC",
-    database: "remotemysql.com"
+    database: "jMN8bA3G1E"
 });
 
 module.exports = con ;
